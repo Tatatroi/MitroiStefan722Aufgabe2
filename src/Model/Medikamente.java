e8c80682d9283f3aa61644e5f8650facd4f0bac6
Tatatroi <[srefanmitroi@gmail.com]>
2025-01-15 15:11:44 +0200

    modele

---
+++ src/Model/Medikamente.java
@@ -0,0 +1,37 @@
+package Model;
+
+public class Medikamente {
+    public String name;
+    public int Preis;
+    public String Krankenheit;
+
+    public Medikamente(String name, int preis, String krankenheit) {
+        this.name = name;
+        Preis = preis;
+        Krankenheit = krankenheit;
+    }
+
+    public int getPreis() {
+        return Preis;
+    }
+
+    public void setPreis(int preis) {
+        Preis = preis;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getKrankenheit() {
+        return Krankenheit;
+    }
+
+    public void setKrankenheit(String krankenheit) {
+        Krankenheit = krankenheit;
+    }
+}
---
+++ src/Model/Patienten.java
@@ -0,0 +1,60 @@
+package Model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Patienten {
+    public int id;
+    public String name;
+    public int age;
+    public String diagnose;
+    List<Medikamente> medikamenteList = new ArrayList<Medikamente>();
+
+    public Patienten(int id, String name, int age, String diagnose, List<Medikamente> medikamenteList) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.diagnose = diagnose;
+        this.medikamenteList = medikamenteList;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getDiagnose() {
+        return diagnose;
+    }
+
+    public void setDiagnose(String diagnose) {
+        this.diagnose = diagnose;
+    }
+
+    public List<Medikamente> getMedikamenteList() {
+        return medikamenteList;
+    }
+
+    public void setMedikamenteList(List<Medikamente> medikamenteList) {
+        this.medikamenteList = medikamenteList;
+    }
+}
